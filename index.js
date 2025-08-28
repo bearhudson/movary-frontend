68b2e9969662242a48e461102ddcc20f1002973f
Brian <github@selfdestroyedindustries.com>
2025-08-28 14:10:46 -0400

    Move to port 3000

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const axios = require('axios');
 const app = express();
-const PORT = 80;
+const PORT = 3000;
 
 // Load environment variables from a .env file
 require('dotenv').config();
