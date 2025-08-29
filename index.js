d7ebd9a57ed5630af0f6c9e9a83f030e72ea9d4a
Brian <github@selfdestroyedindustries.com>
2025-08-29 06:22:09 -0400

    fixed

--- index.js
+++ index.js
@@ -151,7 +151,7 @@ class MovaryApp {
         }
 
         res.send(`
-            <-- Handmade with love by Brian Hudson  -->
+            <!-- Handmade with love by Brian Hudson  -->
             <!DOCTYPE html>
             <html lang="en">
             <head>
